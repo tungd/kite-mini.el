a14031de0554b427f5b34f02bd6475870e596df3
Tung Dao <me@tungdao.com>
2014-03-08 16:13:33 +0700

    refactor

--- kite-mini.el
+++ kite-mini.el
@@ -13,14 +13,23 @@
   "FIXME: "
   :group 'kite-mini)
 
+
 (defvar km-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-x C-e") #'km-eval)
+    (define-key map (kbd "C-x C-e") #'km-evaluate-region-or-line)
     (define-key map (kbd "C-x C-u") #'km-update)
     (define-key map (kbd "C-x C-r") #'km-reload))
   "FIXME: ")
 
-(defvar km-connection-history nil)
+(defvar km-socket nil
+  "FIXME: ")
+
+(defvar km-rpc-id 0
+  "FIXME: ")
+
+(defvar km-rpc-callbacks nil
+  "FIXME: ")
+
 
 (defun km-encode (data)
   (let ((json-array-type 'list)
@@ -43,14 +52,28 @@
         (goto-char (+ 1 url-http-end-of-headers))
         (json-read)))))
 
-(defun km-page-tab? (tab)
-  (string-equal (plist-get tab :type) "page"))
+
+(defun km-next-rpc-id ()
+  (setq km-rpc-id (+ 1 km-rpc-id)))
+
+
+(defun km-on-open (socket)
+  (message "Kite: connected."))
+
+(defun km-on-message (socket data)
+  (message "Kite: %s"  data))
+
+(defun km-on-close (socket)
+  (message "Kite: disconnected."))
 
 (defun km-get-tabs (host port)
   (let* ((url (url-parse-make-urlobj
                "http" nil nil host port "/json"))
          (tabs (km-get-json url)))
-    (-filter #'km-page-tab? tabs)))
+    (-filter (lambda (tab)
+               (and (plist-get tab :webSocketDebuggerUrl)
+                    (string-equal (plist-get tab :type) "page")))
+             tabs)))
 
 (defun km-tab-completion (tab)
   (let ((title (plist-get tab :title))
@@ -61,15 +84,59 @@
   (let* ((tabs (mapcar #'km-tab-completion
                        (km-get-tabs host port)))
          (selection (completing-read
-                     "Tab: " tabs
-                     nil t "" 'km-connection-history (caar tabs)))
+                     "Tab: " tabs nil t "" nil (caar tabs)))
          (tab (cdr (assoc selection tabs))))
     (plist-get tab :webSocketDebuggerUrl)))
 
+(defun km-call-rpc (method &optional params)
+  (websocket-send-text
+   km-socket
+   (km-encode (list :id (km-next-rpc-id)
+                    :method method
+                    :params params))))
+
+(defun km-open-socket (url)
+  (websocket-open socket-url
+                  :on-open #'km-on-open
+                  :on-message #'km-on-message))
+
 (defun km-connect ()
   (interactive)
-  (let ((socket-url (km-select-tab km-remote-host km-remote-port)))
-    socket-url))
+  (let* ((socket-url (km-select-tab km-remote-host
+                                    km-remote-port)))
+    (setq km-socket (km-open-socket socket-url))
+    (km-call-rpc "Console.enable")
+    (km-call-rpc "Debugger.enable")
+    (km-call-rpc "Network.setCacheDisabled" '(:cacheDisabled t))))
+
+;; Runtime.getProperties
+
+(defun km-send-eval (code)
+  (km-call-rpc
+   "Runtime.evalue"
+   (list :expression code
+         :returnByValue t)))
+
+(defun km-update ()
+  (km-call-rpc
+   "Debugger.setScriptSource"
+   (list :scriptId id
+         :scriptSource code)))
+
+(defun km-reload ()
+  (km-call-rpc
+   "Page.reload"
+   (list :ignoreCache t)))
+
+(defun km-evaluate-region-or-line ()
+  (interactive "*P")
+  (let ((start (if (region-active-p)
+                   (region-beginning)
+                 (line-beginning-position)))
+        (end (if (region-active-p)
+                 (region-end)
+               (line-end-position))))
+    (km-evaluate (buffer-substring-no-properties start end))))
 
 
 (provide 'kite-mini)
